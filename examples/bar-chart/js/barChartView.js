77977c2ca8726ae4fabf8bcee4de0f1afd1054ae
shall <hall.steven.g@gmail.com>
2014-06-22 21:49:40 -0700

    update chart format

--- examples/bar-chart/js/barChartView.js
+++ examples/bar-chart/js/barChartView.js
@@ -1,17 +1,113 @@
+
 define(function(require, exports, module) {
-    var View = require('famous/core/View');
-    var Surface = require('famous/core/Surface');
-    var Transform = require('famous/core/Transform');
-    var StateModifier = require('famous/modifiers/StateModifier');
+  var d3            = require('d3/d3');
+  var View          = require('famous/core/View');
+  var Surface       = require('famous/core/Surface');
+  var Transform     = require('famous/core/Transform');
+  var StateModifier = require('famous/modifiers/StateModifier');
+  var Easing        = require('famous/transitions/Easing');
+  var Modifier      = require('famous/core/Modifier');
 
-    function ChartView() {
-        View.apply(this, arguments);
-    }
+  var createBarView = function (viewHeight, viewWidth, data) {
+    var tooltip = { w: 90, h: 40 };
+    var margins = {t: 50, r: 20, b: 30, l: 40};
+    var width  = viewWidth  - margins.l - margins.r;
+    var height = viewHeight - margins.t - margins.b;
 
-    ChartView.prototype = Object.create(View.prototype);
-    ChartView.prototype.constructor = ChartView;
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1);
 
-    ChartView.DEFAULT_OPTIONS = {};
+    var y = d3.scale.linear()
+        .range([height, 0]);
 
-    module.exports = ChartView;
+    x.domain(data.map(function (d) { return d.letter; }));
+    y.domain([0, d3.max(data, function (d) { return d.frequency; })]);
+
+    var background = new Surface({
+      size: [viewWidth, viewHeight],
+      properties: {
+        border: '3px solid white',
+        borderRadius: '8px',
+        backgroundColor: '#696758'
+      }
+    });
+
+    var tooltipSurface = new Surface({
+      size: [tooltip.w, tooltip.h],
+      classes: ['tooltip']
+    });
+
+    var tooltipModifier = new StateModifier({
+      origin: [0, 0]
+    });
+
+    var getBar = function (d) {
+      var bar = new Surface({
+        size: [x.rangeBand(), height - y(d.frequency)],
+        content: height - y(d.frequency) > 12 ? (d.letter):'',
+        classes: ['bar'],
+        properties: {
+          textAlign: 'center',
+          color: '#36393B',
+          border: '2px solid white',
+          backgroundColor: '#EEE6AB'
+        }
+      });
+
+      bar.on('mouseover', function (e) {
+        var newX, newY;
+
+        tooltipSurface.setProperties({display: 'inline'});
+        tooltipSurface.setContent("Letter: " + d.letter + "<br/>" + d3.format('.2p')(d.frequency));
+
+        newX = x(d.letter) + margins.l - (tooltip.w / 2) + (x.rangeBand() / 2);
+        newY = y(d.frequency) + margins.t - tooltip.h - 15;
+
+        tooltipModifier.setTransform(
+          Transform.translate(newX, newY, 2),
+          { duration : 50, curve: Easing.outCirc }
+        );
+
+        this.setProperties({
+          backgroundColor: '#C5BC8E'
+        });
+      });
+
+      bar.on('mouseout', function() {
+        tooltipSurface.setProperties({display: 'none'});
+        this.setProperties({
+          backgroundColor: '#EEE6AB'
+        });
+      });
+
+      return bar;
+    };
+
+    var getBarModifier = function (d, i) { 
+      var modifier = new StateModifier();
+      modifier.setTransform(
+        Transform.translate(x(d.letter) + 1000, y(d.frequency) + margins.t, 1),
+        { duration : 0 , curve: Easing.inOutElastic }
+      );
+      modifier.setTransform(
+        Transform.translate(x(d.letter) + margins.l, y(d.frequency) + margins.t, 1),
+        { duration : i * 100 + 30, curve: Easing.inOutElastic }
+      );
+
+      return modifier;
+    };
+
+    var view = new View({size: [viewWidth, viewHeight]});
+    view.add(background);
+
+    data.forEach(function (item, index) {
+      view.add(getBarModifier(item, index)).add(getBar(item));
+    });
+
+    view.add(tooltipModifier).add(tooltipSurface);
+
+    return view;
+  };
+  
+  module.exports = createBarView;
 });
--- examples/bar-chart/js/main.js
+++ examples/bar-chart/js/main.js
@@ -6,21 +6,22 @@ define(function(require, exports, module) {
     var Scrollview    = require('famous/views/Scrollview');
     var StateModifier = require('famous/modifiers/StateModifier');
     var ViewSequence  = require('famous/core/ViewSequence');
-    var barChart      = require('barChart');
+    var barChartView  = require('barChartView');
     var d3            = require('d3/d3');
 
-    var el = document.getElementById('chart-div');
-    var mainContext = Engine.createContext(el);    
+    var el = document.getElementById("charts");
+    var mainContext = Engine.createContext(el);
     mainContext.setPerspective(500);
-    var surfaces = [];
+
+    var chartViews = [];
     var scrollview = new Scrollview({
-        margin: 40
+        margin: 50
     });
 
     Engine.pipe(scrollview);
 
     var viewSequence = new ViewSequence({
-        array: surfaces,
+        array: chartViews,
         loop: true
     });
     scrollview.sequenceFrom(viewSequence);
@@ -38,62 +39,12 @@ define(function(require, exports, module) {
     d3.csv('data/letters.csv', function (err, data) {
 
       for (var i = 0; i < 30; i++) {
-          var surface = barChart(400, 800, 'letter', 'frequency', data, mainContext);
-
-          surfaces.push(surface);
+          var view = barChartView(400, 800, data);
+          chartViews.push(view);
       }
 
       scrollview.outputFrom(function(offset) {
           return Transform.moveThen([0, -50, 350], Transform.rotateX(-0.004 * offset));
       });
     });
-
-
-
-
-
-
-    // var d3               = require('d3/d3');
-    // var Engine           = require('famous/core/Engine');
-    // var Modifier         = require('famous/core/Modifier');
-    // var Transform        = require('famous/core/Transform');
-    // var Surface          = require('famous/core/Surface');
-    // var barChart         = require('barChart');
-    // var barView          = require('barChartView');
-    // var SequentialLayout = require("famous/views/SequentialLayout");
-
-    // var el = document.getElementById('chart-div');
-    // var mainContext = Engine.createContext(el);
-
-    // var sequentialLayout = new SequentialLayout();
-
-    // var renderables = [];
-
-    // sequentialLayout.sequenceFrom(renderables);
-
-    // d3.csv('data/letters.csv', function (err, data) {
-    //   console.log("yo")
-    //   var view = new barView({
-    //     size: [800, 500]
-    //   });
-
-    //   var chart1 = barChart(100, 800, 'letter', 'frequency', data, mainContext);
-    //   var chart2 = barChart(100, 800, 'letter', 'frequency', data, mainContext);
-
-    //   chart1.getSize = function () {
-    //     return [800, 100]
-    //   }
-
-    //   chart2.getSize = function () {
-    //     return [800, 100]
-    //   }
-
-
-    //   console.log(chart2.getSize());
-    //   renderables.push(chart1, chart2);
-    //   // BubbleChart(500, 800, 'comb', 'frequency', data['Fuel Economy'], mainContext);
-    //   // BubbleHierarchy(500, 800, 'comb', ['make'], data['Fuel Economy'], mainContext);
-
-    // });
-    // mainContext.add(sequentialLayout);
 });
