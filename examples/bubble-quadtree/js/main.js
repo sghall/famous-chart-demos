0e0b57ed3da7e8d1d1d3e314d71f0db2528ce4eb
shall <hall.steven.g@gmail.com>
2014-06-24 11:59:51 -0700

    working quadtree

--- examples/bubble-quadtree/js/main.js
+++ examples/bubble-quadtree/js/main.js
@@ -13,49 +13,23 @@ define(function(require, exports, module) {
   var Collisions     = require('famous/physics/constraints/Collision');
   var Walls          = require('famous/physics/constraints/Walls');
   var StateModifier  = require('famous/modifiers/StateModifier');
+  var Easing         = require('famous/transitions/Easing')
   var Vector         = require('famous/math/Vector');
   var d3             = require('d3/d3');
 
   var el = document.getElementById("charts");
-  var mainContext = Engine.createContext();
+  var mainContext = Engine.createContext(el);
   mainContext.setPerspective(500);
-  window.PE = new PhysicsEngine();
-  var collision = new Collisions({restitution : .75});
-  window.drag = new Drag({strength : .0005});
-
 
   d3.csv('data/fuel.csv', function (error, data) {
     window.data = data;
     var width = 1000, height = 1000;
+    var force = d3.layout.force()
+      .charge(0)
+      .gravity(0)
+      .size([width, height])
+
     var fill = d3.scale.ordinal().range(['#827d92','#827354','#523536','#72856a','#2a3285','#383435'])
-    var svg = d3.select("#charts").append("svg")
-        .attr("width", width)
-        .attr("height", height);
-
-    for (var j = 0; j < data.length; j++) {
-      data[j].radius = +data[j].comb / 2;
-      data[j].x = Math.random() * 100 + 200;
-      data[j].y = Math.random() * 100 + 200;
-    }
-
-    var getCenters = function (vname, w, h) {
-      var nodes = _.uniq(_.pluck(data, vname)).map(function (d) {
-        return {name: d, value: 1};
-      });
-
-      var map = d3.layout.treemap().size([w, h]).ratio(1/1);
-      map.nodes({children: nodes});
-
-      return nodes;
-    }
-
-    var getVectorField = function (x, y) {
-      return new VectorField({
-        field : VectorField.FIELDS.POINT_ATTRACTOR, 
-        strength : 0.00002,
-        position: new Vector(x, y, 0)
-      });
-    };
 
     var getSurface = function (d) {
       return new Surface({
@@ -69,52 +43,72 @@ define(function(require, exports, module) {
       });
     };
 
-    var getCircle = function (d) {
-      return new Circle({
-        radius: d.radius,
-        position: [d.x, d.y, 0]
+    var getSurfaceModifier = function (d, i) {
+      // console.log(i, d)
+      var modifier = new StateModifier({
+        align: [0, 0],
+        origin: [0.5, 0.5]
       });
+
+      modifier.setTransform(
+        Transform.translate(d.x, d.y, 1),
+        { duration : 0, curve: Easing.inOutElastic }
+      );
+
+      return modifier;
     };
 
-    var targets = [];
-    for (var k = 0; k < data.length; k++) {
-      var d = data[k];
-      var particle = getCircle(d);
-      var surface  = getSurface(d);
-      PE.addBody(particle);
-      mainContext.add(particle).add(surface);
-      targets.push(particle);
+    for (var j = 0; j < data.length; j++) {
+      data[j].radius = +data[j].comb / 2;
+      data[j].x = Math.random() * 100 + 200;
+      data[j].y = Math.random() * 100 + 200;
+      data[j].surface = getSurface(data[j]);
+      data[j].modifier = getSurfaceModifier(data[j], j);
+      mainContext.add(data[j].modifier).add(data[j].surface);
     }
 
-    startPhysics('make');
+    var padding = 2;
+    var maxRadius = d3.max(_.pluck(data, 'radius'));
 
-    function startPhysics (varname) {
+
+    console.log(data);
+
+    var getCenters = function (vname, w, h) {
+      var nodes = _.uniq(_.pluck(data, vname)).map(function (d) {
+        return {name: d, value: 1};
+      });
+
+      var map = d3.layout.treemap().size([w, h]).ratio(1/1);
+      map.nodes({children: nodes});
+
+      return nodes;
+    }
+
+    draw('make');
+
+    function draw (varname) {
       var centers = getCenters(varname, 600, 600);
+      force.on("tick", tick(centers, varname, .85));
+      force.start();
+    }
+
+    function tick (centers, varname, k) {
       var foci = {}
       for (var i = 0; i < centers.length; i++) {
-        var center = foci[centers[i].name]= centers[i];
-        var x = center.dx / 2 + center.x;
-        var y = center.dy / 2 + center.y;
-        center.gravity = getVectorField(x, y);
+        foci[centers[i].name] = centers[i];
       }
-      console.log(foci);
-      for (var d = 0, len = targets.length; d < len; d++) {
-        var gravity = foci[data[d][varname]].gravity;
-        PE.attach([collision], targets, targets[d]);
-        PE.attach([gravity, drag], targets[d]);
+      return function (e) {
+        for (var d = 0, len = data.length; d < len; d++) {
+          var target = foci[data[d][varname]];
+          data[d].y += (target.y - data[d].y) * k * e.alpha;
+          data[d].x += (target.x - data[d].x) * k * e.alpha;
+          collide(0.5)(data[d]);
+          data[d].modifier.setTransform(
+            Transform.translate(data[d].x, data[d].y, 10),
+            { duration : 10, curve: 'linear' }
+          )
+        }
       }
-
-      // setTimeout(function () {
-      //   var agents = PE._agents;
-      //   for (var a in agents) {
-      //     if (agents[a].agent instanceof Drag) {
-      //       console.log(+a, agents[a])
-      //       PE.detach(+a);
-      //     }
-      //   }
-      // }, 10000);
-
-
     }
 
     function collide(alpha) {
